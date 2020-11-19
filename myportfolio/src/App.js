dd40a60e0f54acea4b3f2202646e7b02ee738f2c
mora5925 <wahba190@gmail.com>
2020-11-19 09:53:37 -0500

    navbar

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,11 +1,31 @@
 import React, { component } from 'react';
 import './App.css';
-
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 function App() {
   return (
-    <div className="App">
-     <h1> yalla</h1>
-    </div>
+    <div className="demo-big-content">
+    <Layout>
+        <Header title="Title" scroll>
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+        </Content>
+    </Layout>
+</div>
   );
 }
 
